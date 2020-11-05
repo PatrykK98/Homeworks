d3aff79a6606206f5a79109b414cee47a010ca63
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2020-11-05 10:46:34 +0100

    Update Word_split.java

--- Word_split.java
+++ Word_split.java
@@ -2,15 +2,15 @@
 public class Word_split {
 	public static void main(String[] args) {
 		System.out.println("toCharArray() METHOD: \n");
-		char [] letters=args[0].toCharArray();
-		for(char i=0;i<=letters.length-1;i++) {
-			System.out.println(letters[i]);
+		char[] letters = args[0].toCharArray();
+		for (char i : letters) {
+			System.out.println(i);
 		}
-		
+
 		System.out.println("\ncharAt(index) METHOD: \n");
-		String word=args[0].toString();
-		for(int j=0;j<=word.length()-1;j++) {
+		String word = args[0].toString();
+		for (int j = 0; j <= word.length() - 1; j++) {
 			System.out.println(word.charAt(j));
-		}	
+		}
 	}
 }

b64a6936b875f9e4883d7fb60840230c49f24ad9
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2020-11-06 16:16:37 +0100

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,18 +1,26 @@
 import java.util.Arrays;
 
 public class BubbleSort {
-	public static void main(String[] args) {
-		int [] array = {9,1,8,2,7,3,6,4,5};
-		for(int i=0;i<=array.length-1;i++) {
-			for(int j=0;j<=array.length-i-1;j++) {
-				if(array[j]>array[j+1]){
-					int temp = array[j];
-					array[j]=array[j+1];
-					array[j+1]=temp;
-					System.out.println(Arrays.toString(array));
+	public static void BubbleSort(int[] array) {
+			for (int i = 0; i < array.length - 1; i++) {
+				for (int j = 0; j < array.length - i - 1; j++) {
+					if (array[j] > array[j + 1]) {
+						int temp = array[j];
+						array[j] = array[j + 1];
+						array[j + 1] = temp;	
+					}
 				}
 			}
+	}
+	public static void printArray(int []array) {
+		for(int i=0;i<array.length;i++) {
+			System.out.print(array[i]+" ");
 		}
-	
+	}
+	public static void main(String[] args) {
+		int[] a = { 9, 8, 7, 6, 5, 3, 4, 2, 1, 0};
+		BubbleSort(a);
+		printArray(a);
+
 	}
 }

b88868368c4432bb3601fc5236c23969ad80a16a
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2020-11-17 22:23:01 +0100

    Update CarTester.java

--- Interfaces/CarTester.java
+++ Interfaces/CarTester.java
@@ -3,23 +3,22 @@ package CarWithInterfaces;
 public class CarTester {
 
 public static void main(String[] args) {
-	FuelCar volk = new FuelCar("Volksvagen","R32");
-	FuelCar mazd = new FuelCar("Mazda","rx-8");
-	ElectricCar eVolk = new ElectricCar("Volkswagen","UP!");
-	ElectricCar eMazd = new ElectricCar("Mazda","MX-30");
-	HybridCar hVolk = new HybridCar("Volkswagen","Golf");
-	HybridCar hMazd = new HybridCar("Mazda","Axel");
+	Car volk = new FuelCar("Volksvagen","R32");
+	Car mazd = new FuelCar("Mazda","rx-8");
+	Car eVolk = new ElectricCar("Volkswagen","UP!");
+	Car eMazd = new ElectricCar("Mazda","MX-30");
+	Car hVolk = new HybridCar("Volkswagen","Golf");
+	Car hMazd = new HybridCar("Mazda","Axel");
 	
 	Car [] cars = {volk, eVolk, hVolk, mazd, eMazd, hMazd};
+	
 	for(int i=0;i<=cars.length-1;i++) {
-		
-		if((Tankable)cars[i] != null) {
-			((Tankable)cars[i]).tank();
-		}
-		else if((Chargable)cars[i] != null) { 
-			((Chargable)cars[i]).charge();
-		}
-		
+	if(cars[i].chargable) 
+		cars[i].charge();
+	
+	 if(cars[i].tankable) 
+		cars[i].tank();
 	}
+	
 }
 }

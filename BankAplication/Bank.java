2c4ef02a3fc33f238e96a3cec2f98cd1162b32eb
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2020-12-14 18:49:13 +0100

    Update Bank.java

--- BankAplication/Bank.java
+++ BankAplication/Bank.java
@@ -38,5 +38,29 @@ public class Bank {
 	public String toString() {
 		return "Bank [name=" + name + accountList + "]";
 	}
+		@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bank other = (Bank) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 
 }

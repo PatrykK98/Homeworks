d41585ed851bde1be06cc46b83d86b27ab11f8c0
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2020-12-15 17:20:33 +0100

    Update Book.java

--- Generics/Book.java
+++ Generics/Book.java
@@ -1,4 +1,14 @@
 
 public class Book extends Item {
-
+	private String author;
+	private String title;
+	public Book(String title, String author) {
+		this.title=title;
+		this.author=author;
+	}
+	@Override
+	public String toString() {
+		return title+" written by "+author;
+	}
 }
+

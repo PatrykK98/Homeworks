d5a652669c0eb9405337411b0b4cda8b3d456152
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2020-12-16 16:45:22 +0100

    Update DebitAcc.java

--- BankAplication/DebitAcc.java
+++ BankAplication/DebitAcc.java
@@ -1,13 +1,36 @@
-package Accounts;
+package restricted;
 
-import java.io.IOException;
 import java.math.BigDecimal;
 
-import Banks.Bank;
-
 public class DebitAcc extends Account {
-
+	private BigDecimal limit = BigDecimal.ZERO;
 	public DebitAcc(String number) {
 		super(number);
 	}
+
+	@Override
+	BigDecimal withdraw(BigDecimal amount) {
+		balance = balance.subtract(amount);
+		if (balance.compareTo(limit) == -1) {
+			System.out.println("You can't make the transaction beacause you do not have enough money");
+			balance = balance.add(amount);
+		}
+		if (balance.compareTo(limit) == 1) {
+			return balance;
+		}if(balance.compareTo(limit) == 0) {
+			System.out.println("Balance equals 0");
+		}
+		return balance;
+	}
+	
+	@Override
+	BigDecimal recalculatePercents(BigDecimal percent) {
+		BigDecimal value = balance.subtract(balance.multiply(percent));
+		if(value.compareTo(BigDecimal.ZERO) == -1) {
+			System.out.println("not enough money");
+		}if(balance.compareTo(BigDecimal.ZERO) == 1) {
+			return balance.subtract(balance.multiply(percent));
+	}return balance;
+	
+}
 }

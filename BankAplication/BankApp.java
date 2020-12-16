d32f6004b2b6e753e2ed2f6bf0a54d515bb5abad
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2020-12-16 16:44:41 +0100

    Update BankApp.java

--- BankAplication/BankApp.java
+++ BankAplication/BankApp.java
@@ -2,8 +2,10 @@ package Banks;
 
 import java.math.BigDecimal;
 
-import Accounts.Account;
-import Accounts.CreditAcc;
+import restricted.Account;
+import restricted.Bank;
+import restricted.CreditAcc;
+import restricted.DebitAcc;
 
 public class BankApp {
 
@@ -12,24 +14,37 @@ public class BankApp {
 	}
 
 	public static void main(String[] args) {
-		
+		Bank lloyds = new Bank("Lloyds");
+		Bank alior = new Bank("Alior");
+		Account personal = new CreditAcc("12345", new BigDecimal("-50"));
+		Account corporate = new DebitAcc("54321");
+
 		try {
 			System.out.println(NationalBank.getByName("Lloyds"));
 		} catch (BankNotFoundException e) {
 			System.out.println(e.getMessage());
 		}
+		try {
+			System.out.println(NationalBank.getByName("Alior"));
+		} catch (BankNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 	private static void initBanking() {
 
 		Bank lloyds = new Bank("Lloyds");
 		Bank alior = new Bank("Alior");
-		Account personal = new CreditAcc("12345");
-		lloyds.registerAccount(personal);
+		Account personal = new CreditAcc("12345", new BigDecimal("-50"));
+		Account corporate = new DebitAcc("54321");
+		lloyds.registerCreditAccount(personal);
+		alior.registerDebitAccount(corporate);
 		NationalBank.registerBank(lloyds);
 		NationalBank.registerBank(alior);
-		lloyds.topUp("12345", new BigDecimal("450"));
-		lloyds.withdraw("12345", new BigDecimal("125"));
+		alior.topUp("54321", new BigDecimal("100"));
+		alior.withdraw("54321", new BigDecimal("100"));
+		System.out.println(alior.recalculatePercents("54321",new BigDecimal("0.2")));
 
 	}
 

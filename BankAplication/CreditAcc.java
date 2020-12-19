29d69bf80e48aa9a0d35ea9deba5e39d1e08c980
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2020-12-19 18:59:43 +0100

    Update CreditAcc.java

--- BankAplication/CreditAcc.java
+++ BankAplication/CreditAcc.java
@@ -2,6 +2,8 @@ package restricted;
 
 import java.math.BigDecimal;
 
+import ExceptionClasses.ReachedCreditLimitException;
+
 public class CreditAcc extends Account {
 	BigDecimal limit;
 
@@ -10,17 +12,18 @@ public class CreditAcc extends Account {
 		this.limit=limit;
 	}
 	@Override
-	BigDecimal withdraw(BigDecimal amount) {
+	BigDecimal withdraw(BigDecimal amount) throws ReachedCreditLimitException  {
 		balance = balance.subtract(amount);
-		if (balance.compareTo(limit) == -1) {
-			System.out.println("You can't make the transaction beacause you do not have enough money");
+		if (balance.compareTo(limit) == 0){
+			throw new ReachedCreditLimitException("You reached the limit which is: "+limit);
+		}
+		if (balance.compareTo(limit) == -1){
 			balance = balance.add(amount);
+			throw new ReachedCreditLimitException("Can't make the transaction bacause you reached the limit, which is: "+limit+"\nYour balance is: "+balance);
 		}
 		if (balance.compareTo(limit) == 1) {
+			System.out.println("Your balance is: ");
 			return balance;
-		}
-		if(balance.compareTo(limit) == 0) {
-			System.out.println("You reached the limit which is: "+limit);
 		}
 		return balance;
 	}

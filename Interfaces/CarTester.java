ea8e124f24492889a98e4e8188183367a3899385
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2021-01-02 18:44:41 +0100

    Update CarTester.java

--- Interfaces/CarTester.java
+++ Interfaces/CarTester.java
@@ -2,29 +2,27 @@ package CarWithInterfaces;
 
 public class CarTester {
 
-public static void main(String[] args) {
-	Car hVolk = new HybridCar("Volkswagen","Golf");
-	Car hMazd = new HybridCar("Mazda","Axel");
-	Car volk = new FuelCar("Volksvagen","R32");
-	Car mazd = new FuelCar("Mazda","rx-8");
-	Car eVolk = new ElectricCar("Volkswagen","UP!");
-	Car eMazd = new ElectricCar("Mazda","MX-30");
+	public static void main(String[] args) {
+		Car hVolk = new HybridCar("Smart", "City");
+		Car hMazd = new HybridCar("Smart", "Country");
+		Car volk = new FuelCar("Volksvagen", "R32");
+		Car mazd = new FuelCar("Mazda", "rx-8");
+		Car eVolk = new ElectricCar("Volkswagen", "UP!");
+		Car eMazd = new ElectricCar("Mazda", "MX-30");
 
-	
-	Car [] cars = {volk, eVolk, hVolk, mazd, eMazd, hMazd};
-	
-	for(int i=0;i<=cars.length-1;i++) {
+		Car[] cars = { volk, eVolk, hVolk, mazd, eMazd, hMazd };
 
-	if(cars[i] instanceof FuelCar) {
-		((FuelCar) cars[i]).tank();
+		for (int i = 0; i <= cars.length - 1; i++) {
+			if (cars[i] instanceof HybridCar) {
+				((HybridCar) cars[i]).charge();
+				((HybridCar) cars[i]).tank();
+			}
+			if (cars[i] instanceof FuelCar) {
+				((FuelCar) cars[i]).tank();
+			}
+			if (cars[i] instanceof ElectricCar) {
+				((ElectricCar) cars[i]).charge();
+			}
+		}
 	}
-	if(cars[i] instanceof HybridCar) {
-		((HybridCar)cars[i]).charge();
-		((HybridCar)cars[i]).tank();
-	}
-	if(cars[i] instanceof ElectricCar) {
-		 ((ElectricCar) cars[i]).charge();
-	}
-}
-}
 }

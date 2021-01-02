5958f71f8b6e8cf6e8606662c50cf20b1031f1bd
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2021-01-02 17:33:44 +0100

    Update CreditAcc.java

--- BankAplication/CreditAcc.java
+++ BankAplication/CreditAcc.java
@@ -13,30 +13,31 @@ public class CreditAcc extends Account {
 	}
 	@Override
 	BigDecimal withdraw(BigDecimal amount) throws ReachedCreditLimitException  {
-		balance = balance.subtract(amount);
-		if (balance.compareTo(limit) == 0){
+		setBalance(getBalance().subtract(amount)); 
+		if (getBalance().compareTo(limit) == 0){
 			System.out.println("You reached the limit which is: "+limit);
 		}
-		if (balance.compareTo(limit) == -1){
-			balance = balance.add(amount);
-			throw new ReachedCreditLimitException("Can't make the transaction bacause you reached the limit, which is: "+limit+"\nYour balance is: "+balance);
+		if (getBalance().compareTo(limit) == -1){
+			setBalance(getBalance().add(amount));
+			throw new ReachedCreditLimitException("Can't make the transaction bacause you reached the limit, which is: "+limit+"\nYour balance is: "+getBalance());
 		}
-		if (balance.compareTo(limit) == 1) {
-			System.out.println("Your balance is: ");
-			return balance;
+		if (getBalance().compareTo(limit) == 1) {
+			System.out.println("Your balance is: "+getBalance());
 		}
-		return balance;
+		return getBalance();
 	}
 	
 	@Override
 	BigDecimal recalculatePercents(BigDecimal percent) {
-		if(balance.compareTo(BigDecimal.ZERO) == 1) {
-		return balance.subtract(balance.multiply(percent));
+		if(getBalance().compareTo(BigDecimal.ZERO) == 1) {
+		setBalance(getBalance().subtract(getBalance().multiply(percent)));
+		return getBalance();
 		}
-		else if((balance.compareTo(BigDecimal.ZERO) == -1)){
-			return balance.add(balance.multiply(percent));
+		else if((getBalance().compareTo(BigDecimal.ZERO) == -1)){
+			 setBalance(getBalance().add(getBalance().multiply(percent)));
+			 return getBalance();
 		}
-		return balance;
+		return getBalance();
 	}
 	
 

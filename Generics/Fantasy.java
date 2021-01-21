ca73e5d31f520a086bb1da61c27f4ca0cf7da3ce
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2021-01-21 18:19:43 +0100

    Update Fantasy.java

--- Generics/Fantasy.java
+++ Generics/Fantasy.java
@@ -1,14 +1,4 @@
+package BoxesAndShelves;
 
-public class Fantasy extends Book {
-
-	public Fantasy(String title, String author) {
-		super(title, author);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public String toString() {
-		return "Fantasy book named: " + super.toString();
-	}
-	
+public class Fantasy extends Book{
 }

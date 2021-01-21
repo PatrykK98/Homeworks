380c3b7e990b0df561a1692a4aca6252e359ef04
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2021-01-21 18:18:59 +0100

    Update Book.java

--- Generics/Book.java
+++ Generics/Book.java
@@ -1,14 +1,4 @@
+package BoxesAndShelves;
 
-public class Book extends Item {
-	private String author;
-	private String title;
-	public Book(String title, String author) {
-		this.title=title;
-		this.author=author;
-	}
-	@Override
-	public String toString() {
-		return title+" written by "+author;
-	}
+public class Book extends Item{
 }
-

8de59f1541d3d5a57594efa4470a7edbb1d035c8
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2021-01-21 18:20:08 +0100

    Update Food.java

--- Generics/Food.java
+++ Generics/Food.java
@@ -1,12 +1,4 @@
+package BoxesAndShelves;
 
-public class Food extends Item {
-private String name;
-public Food(String name) {
-	this.name=name;
-}
-@Override
-public String toString() {
-	return name;
-}
-
+public class Food extends Item{
 }

58613f5a4b45227958780ddf1ebadcc9bc989646
PatrykK98 <73220222+PatrykK98@users.noreply.github.com>
2021-01-21 18:21:22 +0100

    Update ShelfDemo.java

--- Generics/ShelfDemo.java
+++ Generics/ShelfDemo.java
@@ -1,48 +1,40 @@
-import java.util.ArrayList;
+package BoxesAndShelves;
 
-public class ShelfDemo {
-	public static void main(String[] args) {
+public class Tester {
+    public static void main(String[] args) {
 
-		Fantasy fBook1 = new Fantasy("Clean Code","Robert C.Martin");
-		Fantasy fBook2 = new Fantasy("Harry Poter", "Rowling");
-		Book book1 = new Book("Shining", "Stephen King");
-		Book book2 = new Book("Superman", "Antonio Banderas");
-		Toy toy1 = new Toy("Hootwheels");
-		Toy toy2 = new Toy("Remoted car");
-		Food food1 = new Food("zapiekanka");
-		Food food2 = new Food("frytki"); 
-		
-		Box<Toy> toyBox = new Box<>();
-		toyBox.add(toy1);
-		toyBox.add(toy2);
-		
-		Box<Book> booksBox = new Box<>();
-		booksBox.add(fBook1);
-		booksBox.add(fBook2);
-		booksBox.add(book1);
-		booksBox.add(book2);
-		
-		Box<Fantasy> fBookBox = new Box<>();
-		fBookBox.add(fBook1);
-		fBookBox.add(fBook2);
-	
-		Box<Food> fBox = new Box<>();
-		fBox.add(food1);
-		fBox.add(food2);
-		
-		Shelf<Book>bookShelf = new Shelf<>();
-		bookShelf.add(booksBox);
-		bookShelf.add(fBookBox);
-		
-		Shelf<Toy> toyShelf = new Shelf<>();
-		toyShelf.add(toyBox);
-		
-		Shelf<Item> stuffShelf = new Shelf<>();
-		stuffShelf.add(toyBox);
-		stuffShelf.add(fBookBox);
-		stuffShelf.add(fBox);
-		stuffShelf.add(booksBox);
-		
-		fBookBox.getItems();
-	}
+        Food chicken = new Food();
+        Toy car = new Toy();
+        Toy doll = new Toy();
+        Book criminal = new Book();
+        Fantasy fantasy = new Fantasy();
+
+        Box<Food> foodBox = new Box();
+        Box<Toy> toyBox = new Box();
+        Box<Book> bookBox = new Box();
+        Box<Fantasy> fantasyBox = new Box();
+
+        foodBox.add(chicken);
+        toyBox.add(car);
+        toyBox.add(doll);
+        bookBox.add(criminal);
+        fantasyBox.add(fantasy);
+
+        Shelf<Toy> toyShelf = new Shelf();
+        toyShelf.add(toyBox);
+
+
+        Shelf<Book> bookShelf = new Shelf();
+        bookShelf.add(bookBox);
+        bookShelf.add(fantasyBox);
+
+        Shelf<Item> stuffShelf = new Shelf();
+        stuffShelf.add(toyBox);
+        stuffShelf.add(bookBox);
+        stuffShelf.add(fantasyBox);
+        stuffShelf.add(foodBox);
+
+        System.out.println(toyBox.getItem());
+        System.out.println(stuffShelf.getBoxes());
+    }
 }
